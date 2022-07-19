8bf6440a3bc5f14c0b411faa6ce8433e42fa1292
jpcp0614 <jpcp0614@gmail.com>
2022-07-19 12:16:48 -0300

    Canais

---
+++ Curso-Introducao-GoLang/intro/canais.go
@@ -0,0 +1,24 @@
+// Canais
+
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int) // criei um canal, espaço na memória para armazenar o resultado
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
