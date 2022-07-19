b090e7a088b81d86123c8a31449469a4d513f3c7
jpcp0614 <jpcp0614@gmail.com>
2022-07-19 17:39:24 -0300

    Exercise06

---
+++ Curso-Aprenda-Go/cap3-exercises-nivel-1/exercise06.go
@@ -0,0 +1,19 @@
+/*
+Prova!
+- Link: https://forms.gle/yutXCZCjsgBzMfxK8
+- Se você deu pausa e fez todos os exercícios anteriores você mesmo,
+	e só viu a resposta depois... e se você der pausa agora e fizer a
+	prova inteira por conta própria, e só assistir as respostas depois...
+	sabe o que isso quer dizer? Que você é ninja. Ninja nível 1.
+	Tá no caminho certo pra ser um developer ninja mestre.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("")
+}
