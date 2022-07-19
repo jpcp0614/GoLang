901e6c80585294c3d324b271d6fc041d243a41c2
jpcp0614 <jpcp0614@gmail.com>
2022-07-19 19:58:20 -0300

    Sistemas numéricos

---
+++ Curso-Aprenda-Go/cap4-fundamentos-da-programacao/sistemasNumericos.go
@@ -0,0 +1,16 @@
+/*
+- Base-10: decimal, 0–9
+- Base-2: binário, 0–1
+- Base-16: hexadecimal, 0–f
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 100
+	fmt.Printf("%v \t %b \t %#x", a, a, a)
+}
