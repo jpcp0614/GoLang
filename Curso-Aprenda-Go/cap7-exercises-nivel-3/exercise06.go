0f1347e2aaad3ba56a5aa68a409111570a7eb00a
jpcp0614 <jpcp0614@gmail.com>
2022-07-20 08:23:55 -0300

    Exercise06

---
+++ Curso-Aprenda-Go/cap7-exercises-nivel-3/exercise06.go
@@ -0,0 +1,17 @@
+/*
+Crie um programa que demonstre o funcionamento da declaração if.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	exausto := true
+
+	if exausto {
+		fmt.Println("Cansado")
+	}
+}
