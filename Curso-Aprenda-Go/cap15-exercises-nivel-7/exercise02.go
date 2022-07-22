25830741113a9aab201051289f4e60b655738c0a
jpcp0614 <jpcp0614@gmail.com>
2022-07-22 15:37:01 -0300

    Exercise02

---
+++ Curso-Aprenda-Go/cap15-exercises-nivel-7/exercise02.go
@@ -0,0 +1,40 @@
+/*
+Crie um struct "pessoa"
+- Crie uma função chamada mudeMe que tenha *pessoa como parâmetro. Essa função deve mudar um valor armazenado no endereço *pessoa.
+    - Dica: a maneira "correta" para fazer dereference de um valor em um struct seria (*valor).campo
+    - Mas consta uma exceção na documentação. Link: https://golang.org/ref/spec#Selectors
+    - "As an exception, if the type of x is a named pointer type and (*x).f is a valid selector expression denoting a field (but not a method), 
+    - → x.f is shorthand for (*x).f." ←
+    - Ou seja, podemos usar tanto o atalho p1.nome quanto o tradicional (*p1).nome
+- Crie um valor do tipo pessoa;
+- Use a função mudeMe e demonstre o resultado.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+type pessoa struct {
+	nome			string
+	sobrenome	string
+	idade			int
+}
+
+func mudeMe(p *pessoa) {
+	(*p).nome = "Luizinho"
+	p.sobrenome = "Filho"
+} 
+
+func main() {
+	zezinho := pessoa {
+		nome: "Zezinho",
+		sobrenome: "Filho",
+		idade: 20,
+	}
+
+	mudeMe(&zezinho)
+
+	fmt.Println(zezinho)
+}
