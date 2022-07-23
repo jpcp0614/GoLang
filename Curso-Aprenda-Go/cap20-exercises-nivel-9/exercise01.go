62a930b574ca3fca9a4fde5ab5e5a7b9bba9ed91
jpcp0614 <jpcp0614@gmail.com>
2022-07-23 09:08:43 -0300

    Exercise01

---
+++ Curso-Aprenda-Go/cap20-exercises-nivel-9/exercise01.go
@@ -0,0 +1,35 @@
+/*
+Além da goroutine principal, crie duas outras goroutines.
+- Cada goroutine adicional devem fazer um print separado.
+- Utilize waitgroups para fazer com que suas goroutines finalizem antes de o programa terminar.
+*/
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	novasRoutines(100)
+	wg.Wait()
+
+}
+
+
+
+func novasRoutines(i int) {
+	wg.Add(i)
+	for j := 0; j < i; j++ {
+		x := j
+	
+		go func(i int) {
+			fmt.Println("Eu sou a goroutine número:", i+1)
+			wg.Done()
+		}(x)
+	}
+}
